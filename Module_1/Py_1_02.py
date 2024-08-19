a715d173c446dd861112b297f1d8c22b0ce654e7
aanstr <nester1tester@gmail.com>
2024-08-19 13:53:58 +0300

    Module_1

---
+++ Module_1/Py_1_02.py
@@ -0,0 +1,5 @@
+finished_tasks = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_for_task = hours_spent / finished_tasks
+print('Курс: ', course_name, ', всего задач:', finished_tasks, ', затрачено часов: ', hours_spent, ', среднее время выполнения', time_for_task, ' часа.')
