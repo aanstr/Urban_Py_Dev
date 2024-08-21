4b558678ddaa68b0ce6076931743d9aa93468c24
aanstr <nester1tester@gmail.com>
2024-08-21 19:41:19 +0300

    Module_1

---
+++ Module_2/module_2_3.py
@@ -0,0 +1,11 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+count = 0
+while len(my_list) > count:
+    num = my_list[count]
+    count = count +1
+    if num > 0:
+        print(num)
+    elif num == 0:
+        continue
+    else:
+        break
