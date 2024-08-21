cbcfc83dfee5ef83dada21e710488f2357082b64
aanstr <nester1tester@gmail.com>
2024-08-21 12:15:31 +0300

    Module_1

--- Module_1/module_1_2.py
+++ Module_1/module_1_2.py
@@ -2,4 +2,4 @@ finished_tasks = 12
 hours_spent = 1.5
 course_name = 'Python'
 time_for_task = hours_spent / finished_tasks
-print('Курс: ', course_name, ', всего задач:', finished_tasks, ', затрачено часов: ', hours_spent, ', среднее время выполнения', time_for_task, ' часа.')
+print('Курс:', course_name + f', всего задач: {finished_tasks},', f'затрачено часов: {hours_spent},','среднее время выполнения', time_for_task, 'часа.')
