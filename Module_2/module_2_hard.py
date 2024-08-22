c5fc6593bbcdcaeaabf9bd54c13986a9609bbef6
aanstr <nester1tester@gmail.com>
2024-08-22 21:51:25 +0300

    Module_1

--- Module_2/module_2_hard.py
+++ Module_2/module_2_hard.py
@@ -2,17 +2,16 @@ n = int(input('Введите число (3-20): '))
 
 
 def result(n):
-    get_result = []
+    get_result = ''
     for i in range(1, n):
-        pairs = []
+        pairs = ''
         for j in range(1, 20):
             k = i + j
             if i < j and n % k == 0:
-                pairs.append(i)
-                pairs.append(j)
-        get_result.append(pairs)
-    get_result = sorted(get_result)
+                pairs += str(i) + str(j)
+        get_result += pairs
+    get_result = get_result
     return get_result
 
 
-print('Пароль: ', *result(n))
+print('Пароль: ',result(n))
