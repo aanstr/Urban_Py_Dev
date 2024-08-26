d88fba7a0da557e4d88d28f6fb502d965d12bd37
aanstr <nester1tester@gmail.com>
2024-08-26 15:22:00 +0300

    Changes

--- Module_3/module_3_1.py
+++ Module_3/module_3_1.py
@@ -8,19 +8,22 @@ def count_calls():
 
 def string_info(string):
     count_calls()
-    len_ = len(string)
-    tuple_ = len_, string
+    tuple_ = len(string), string.upper(), string.lower()
     return tuple_
 
 
 def is_contains(string, list_to_search):
     count_calls()
-    print(1)
-
+    is_match = False
+    for i in range(len(list_to_search)):
+        if string.lower() == list_to_search[i].lower():
+            is_match = True
+            break
+    return is_match
 
 
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
-print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
 print(calls)
