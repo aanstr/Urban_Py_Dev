41bd8f154ad041b85832fd718804bbc368d0aabf
aanstr <nester1tester@gmail.com>
2024-09-03 14:32:41 +0300

    Changes

--- Module_3/module_3_4.py
+++ Module_3/module_3_4.py
@@ -1,8 +1,7 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     for i in range(len(other_words)):
-        word = other_words[i]
-        if root_word.lower() in word.lower() or word.lower() in root_word.lower():
+        if root_word.lower() in other_words[i].lower() or other_words[i].lower() in root_word.lower():
             same_words.append(other_words[i])
     return same_words
 
