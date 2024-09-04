b90f7a88ce39c54f2ebf4f793aef7eebb4792176
aanstr <nester1tester@gmail.com>
2024-09-04 14:34:07 +0300

    3_hard

--- Module_3/module_3_hard.py
+++ Module_3/module_3_hard.py
@@ -2,50 +2,31 @@ sum_ = 0
 
 
 def calculate_structure_sum(args):
+    global count
     global sum_
-    result_ = 0
     array_ = args
     for i in array_:
         result_ = 0
-        print(i)
-        print(f'Result {sum_}')
         if isinstance(i, int) or isinstance(i, float):
             result_ += i
         elif isinstance(i, str):
             result_ += len(i)
-        elif isinstance(i, list):
-            result_ += calculate_structure_sum(i)
-        elif isinstance(i, tuple):
-            result_ += calculate_structure_sum(i)
-        elif isinstance(i, set):
-            result_ += calculate_structure_sum(i)
         elif isinstance(i, dict):
             for j in i:
-                print(j)
-                i.get(j)
-                result_ += calculate_structure_sum(j)
-    #          str(i.values())
-    #          str(i.values())
-    #      result_ += calculate_structure_sum(i)
-        print(f'Result_ {result_}')
-    sum_ += result_
+                result_ += (i.get(j))
+                result_ += len(j)
+        else:
+            calculate_structure_sum(i)
+        sum_ += result_
     return sum_
 
 
-# 1+2+3+ len('a') +4+ len('b') +5+6+ len('cube') +7+ len('drum') +8+ len('Hello') +2+ len('Urban') + len('Urban2')+35=99
-
 data_structure = [
-    [1, 2, 3],  # 6
-    {'a': 4, 'b': 5},  # 17
-    (6,  # 23
-     {'cube': 7, 'drum': 8}  # 46
-     ),
-    "Hello",  # 51
-    ((), [{(2,  # 53
-            'Urban',  # 58
-            ('Urban2', 35)  # 64, 99
-            )}])
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
-#  print(list(data_structure[1]))
 result = calculate_structure_sum(data_structure)
-print(result)
+print(f'сумма: {result}')
