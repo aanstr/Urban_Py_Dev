ba6efcb50bd04f1bb194947fd3696ff616683bee
aanstr <nester1tester@gmail.com>
2024-10-02 14:21:56 +0300

    Changes

--- Module_6/module_6_2.py
+++ Module_6/module_6_2.py
@@ -2,35 +2,35 @@ class Vehicle:
     __COLOR_VARIANTS = ['blue', 'red', 'green', 'yellow', 'violet', 'orange', 'brown', 'gray', 'black', 'white']
 
     def __init__(self, owner, model, color, power):
-        self.owner = str(owner)
-        self.__model = str(model)
-        self.__color = str(color)
-        self.__engine_power = int(power)
+        self.owner = owner
+        self.__model = model
+        self.__color = color
+        self.__engine_power = power
 
     def get_model(self):
-        print(f'Модель: {self.__model}')
+        return f'Модель: {self.__model}'
 
     def get_horsepower(self):
-        print(f'Мощность двигателя: {self.__engine_power}')
+        return f'Мощность двигателя: {self.__engine_power}'
 
     def get_color(self):
-        print(f'Цвет: {self.__color}')
+        return f'Цвет: {self.__color}'
+
+    def set_color(self, color):
+        if color.lower() in self.__COLOR_VARIANTS:
+            self.__color = color
+        else:
+            print(f'Нельзя сменить цвет на {color}')
 
     def print_info(self):
-        self.get_model()
-        self.get_horsepower()
-        self.get_color()
+        print(self.get_model())
+        print(self.get_horsepower())
+        print(self.get_color())
         print(f'Владелец: {self.owner}')
 
 
 class Sedan(Vehicle):
     __PASSENGERS_LIMIT = 5
-
-    def set_color(self, color):
-        if color.lower() in self._Vehicle__COLOR_VARIANTS:
-            self._Vehicle__color = color
-        else:
-            print(f'Нельзя сменить цвет на {color}')
 
 
 vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
