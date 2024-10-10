4a58f75daeda0ca939079e12884c49d67feafe78
aanstr <nester1tester@gmail.com>
2024-10-10 11:32:17 +0300

    Changes

--- Module_7/module_7_2.py
+++ Module_7/module_7_2.py
@@ -1,13 +1,12 @@
 def custom_write(file_name, strings):
-    file = open(file_name, 'w', encoding='utf-8')
-    strings_positions = {}
-    num_ = 1
-    for str_ in strings:
-        tuple_ = (num_, file.tell())
-        file.write(f'{str_}\n')
-        num_ += 1
-        strings_positions.update({tuple_: str_})
-    file.close()
+    with open(file_name, 'w', encoding='utf-8') as file:
+        strings_positions = {}
+        num_ = 1
+        for str_ in strings:
+            tuple_ = (num_, file.tell())
+            file.write(f'{str_}\n')
+            num_ += 1
+            strings_positions.update({tuple_: str_})
     return strings_positions
 
 
