733aee5223bdbf81dcb8c646a73c6127ec245fe5
aanstr <nester1tester@gmail.com>
2024-10-10 17:16:24 +0300

    Changes

--- Module_7/module_7_3.py
+++ Module_7/module_7_3.py
@@ -1,23 +1,39 @@
 class WordsFinder:
-    __file_names = ()
-    __all_words = {}
-    def __init__(self, *files):
-        self.__file_names = files
+    def __init__(self, *file_names):
+        self.file_names = file_names
 
     def get_all_words(self):
-        return self.__all_words
+        all_words = {}
+        for file_name in self.file_names:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                words = file.read().lower().replace(',', '').replace('.', '').replace(
+                    '=', '').replace('!', '').replace('?', '').replace(
+                    ';', '').replace(':', '').replace(' - ', ' ').split()
+                all_words[file_name] = words
+        return all_words
+
     def find(self, word):
-        for file_ in self.__file_names:
-            with open(file_, encoding='utf_8') as file:
-                if word.lower() in file:
-                    return {f'{file_.name}: {file_.tell}'}
-    def count(self,text):
-        for file_ in self.__file_names:
-            with open(file_, encoding='utf_8') as file:
-                if text.lower() in file:
-                    return {f'{file_.name}: {file_.tell}'}
+        result = {}
+        for file_name, words in self.get_all_words().items():
+            if word.lower() in words:
+                result[file_name] = words.index(word.lower()) + 1  # ибо индексация с 0
+        return result
+
+    def count(self, word):
+        result = {}
+        for file_name, words in self.get_all_words().items():
+            result[file_name] = words.count(word.lower())
+        return result
+
+
+finder1 = WordsFinder('Walt Whitman - O Captain! My Captain!.txt',
+                      'Rudyard Kipling - If.txt',
+                      'Mother Goose - Monday’s Child.txt')
+print(finder1.get_all_words())
+print(finder1.find('the'))
+print(finder1.count('the'))
 
 finder2 = WordsFinder('test_file.txt')
-print(finder2.get_all_words()) # Все слова
-print(finder2.find('TEXT')) # 3 слово по счёту
-print(finder2.count('teXT')) # 4 слова teXT в тексте всего
+print(finder2.get_all_words())  # Все слова
+print(finder2.find('TEXT'))  # 3 слово по счёту
+print(finder2.count('teXT'))  # 4 слова text в тексте всего
