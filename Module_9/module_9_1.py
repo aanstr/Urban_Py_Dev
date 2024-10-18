41712433d27e3268c53c74508638be61681f3b5a
aanstr <nester1tester@gmail.com>
2024-10-18 11:05:00 +0300

    Changes

--- Module_9/module_9_1.py
+++ Module_9/module_9_1.py
@@ -1,8 +1,18 @@
 def apply_all_func(int_list, *functions):
     result = {}
-    for func in functions:
-        result.update({func.__name__: func(int_list)})
-    return result
+    for i in int_list:
+        try:
+            if not isinstance(i, int or float):
+                raise TypeError
+        except TypeError:
+            print("Следует передать список чисел")
+    try:
+        for func in functions:
+            result.update({func.__name__: func(int_list)})
+        return result
+    except TypeError:
+        print("Ошибка данных на входе")
 
 print(apply_all_func([6, 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+print(apply_all_func([6, 'test', 15, 9], max, min))
