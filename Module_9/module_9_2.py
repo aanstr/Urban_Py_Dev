0ad3ae86a6c917d6d85b5a7be409614c2e7f1f95
aanstr <nester1tester@gmail.com>
2024-10-18 11:44:10 +0300

    Changes

--- Module_9/module_9_2.py
+++ Module_9/module_9_2.py
@@ -1,9 +1,9 @@
 first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
 second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
 
-first_result = [len(string) for string in first_strings if len(string) > 5]
+first_result = [len(i) for i in first_strings if len(i) > 5]
 second_result = [(i, j) for i in first_strings for j in second_strings if len(i) == len(j)]
-third_result = {string: len(string) for string in first_strings + second_strings if not len(string) % 2}
+third_result = {i: len(i) for i in first_strings + second_strings if not len(i) % 2}
 
 print(first_result)
 print(second_result)
