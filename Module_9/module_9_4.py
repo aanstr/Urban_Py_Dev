3f4e57fce26b58cc4dc1926cc1452e0faab4389f
aanstr <nester1tester@gmail.com>
2024-10-26 14:21:35 +0300

    Changes
    # Conflicts:
    #	Module_7/Mother Goose - Monday’s Child.txt
    #	Module_7/Rudyard Kipling - If.txt
    #	Module_7/Walt Whitman - O Captain! My Captain!.txt
    #	Module_7/test_file.txt

--- Module_9/module_9_4.py
+++ Module_9/module_9_4.py
@@ -19,3 +19,19 @@ def get_advanced_writer(file_name):
 
 write = get_advanced_writer('example.txt')
 write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+
+# Метод __call__:
+
+class MysticBall:
+    def __init__(self, *words):
+        self.words = words
+
+    def __call__(self):
+        return choice(self.words)
+
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
