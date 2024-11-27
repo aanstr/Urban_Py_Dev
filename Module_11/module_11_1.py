4eefecaa2bb9ab7ea2bc24de2e9109933d2e1843
aanstr <nester1tester@gmail.com>
2024-11-27 15:55:33 +0300

    Changes

---
+++ Module_11/module_11_1.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+def file_import(filename, encoding='cp1251'):
+    df = pd.read_csv(filename, encoding=encoding)
+    return df
+
+
+def arr_(df):
+    arr = np.array([df['Москва'], df['Санкт-Петербург']])
+    print(arr)
+    print(f'Размерность массива: {arr.ndim}')
+    print(f'Строк, столбцов: {arr.shape}')
+    print(f'Элементов: {arr.size}')
+
+
+def plot(df):
+    plt.plot(df['Год'], df['Москва'] / 1000000, label='Москва')
+    plt.plot(df['Год'], df['Санкт-Петербург'] / 1000000, label='Санкт-Петербург')
+    plt.xlabel('Год')
+    plt.ylabel('Население, млн')
+    plt.title('Динамика изменения населения городов')
+    plt.legend()
+    plt.show()
+
+
+def main():
+    filename = 'wiki.csv'
+    file = file_import(filename)
+    arr_(file)
+    print(file)
+    plot(file)
+
+
+if __name__ == '__main__':
+    main()
