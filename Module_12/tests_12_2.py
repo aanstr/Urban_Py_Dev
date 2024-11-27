dd07c1c7e39a1d67a12206957115788c8bfa7c53
aanstr <nester1tester@gmail.com>
2024-11-27 20:13:47 +0300

    Changes

---
+++ Module_12/tests_12_2.py
@@ -0,0 +1,39 @@
+import unittest
+import runner_and_tournament as run
+
+
+class TournamentTest(unittest.TestCase):
+
+    def setUpClass(self):
+        self.all_results = {}
+
+    def setUp(self):
+        self.usain = run.Runner('Усэйн', 10)
+        self.andrey = run.Runner('Андрей', 9)
+        self.nik = run.Runner('Ник', 3)
+
+    def test1(self):
+        race = run.Tournament(90, self.usain, self.nik)
+        result = race.start()
+        self.all_results.update(result)
+        self.assertTrue(max(self.all_results.keys()), 'Ник')
+
+    def test2(self):
+        race = run.Tournament(90, self.andrey, self.nik)
+        result = race.start()
+        self.all_results.update(result)
+        self.assertTrue(max(self.all_results.keys()), 'Ник')
+
+    def test3(self):
+        race = run.Tournament(90, self.usain, self.andrey, self.nik)
+        result = race.start()
+        self.all_results.update(result)
+        self.assertTrue(max(self.all_results.keys()), 'Ник')
+
+    def tearDownClass(self):
+        for race in self.all_results:
+            print(self.all_results.__getitem__(race))
+
+
+if __name__ == '__main__':
+    unittest.main()
