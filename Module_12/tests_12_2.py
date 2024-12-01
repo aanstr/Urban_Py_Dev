6fa21b5ebba786676d72560a6b433ea761f43e73
aanstr <nester1tester@gmail.com>
2024-12-01 11:28:43 +0300

    Changes

--- Module_12/tests_12_2.py
+++ Module_12/tests_12_2.py
@@ -16,32 +16,31 @@ class TournamentTest(unittest.TestCase):
     def test1(self):
         race = run.Tournament(90, self.usain, self.nik)
         result = race.start()
-        self.all_results.update({place: str(runner) for place, runner in result.items()})
-        self.assertTrue(max(self.all_results.keys()), 'Ник')
+        self.all_results[0] = {place: str(runner) for place, runner in result.items()}
+        self.assertTrue(max(result.keys()), 'Ник')
 
     def test2(self):
         race = run.Tournament(90, self.andrey, self.nik)
         result = race.start()
-        self.all_results.update({place: str(runner) for place, runner in result.items()})
-        self.assertTrue(max(self.all_results.keys()), 'Ник')
+        self.all_results[1] = {place: str(runner) for place, runner in result.items()}
+        self.assertTrue(max(result.keys()), 'Ник')
 
     def test3(self):
         race = run.Tournament(90, self.usain, self.andrey, self.nik)
         result = race.start()
-        self.all_results.update({place: str(runner) for place, runner in result.items()})
-        self.assertTrue(max(self.all_results.keys()), 'Ник')
+        self.all_results[2] = {place: str(runner) for place, runner in result.items()}
+        self.assertTrue(max(result.keys()), 'Ник')
 
     def test4(self):
         race = run.Tournament(90, self.nik, self.andrey, self.usain)
         result = race.start()
-        self.all_results.update({place: str(runner) for place, runner in result.items()})
-        self.assertTrue(max(self.all_results.keys()), 'Ник')
+        self.all_results[3] = {place: str(runner) for place, runner in result.items()}
+        self.assertTrue(max(result.keys()), 'Ник')
 
     @classmethod
     def tearDownClass(cls):
-        print(cls.all_results.values())
         for race in cls.all_results:
-            print(cls.all_results.__getitem__(race))
+            print(f'Забег {race+1}: {cls.all_results.__getitem__(race)}')
 
 
 if __name__ == '__main__':
