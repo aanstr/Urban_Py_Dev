f122cc38a2ad0a8370f3d2afe1a03089ca2d5643
aanstr <nester1tester@gmail.com>
2024-12-24 18:48:39 +0300

    Changes

---
+++ Module_14/crud_functions.py
@@ -0,0 +1,31 @@
+import sqlite3
+
+
+def initiate_db():
+    connection = sqlite3.connect('telegram.db')
+    cursor = connection.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    price TEXT NOT NULL
+    )
+    ''')
+    for i in range(1, 5):
+        cursor.execute('INSERT INTO Products (id, title, description, price) VALUES (?, ?, ?, ?)',
+                       (i, f'Product{i}', f'description{i}', i * 100))
+    connection.commit()
+    connection.close()
+
+
+def get_all_products():
+    connection = sqlite3.connect('telegram.db')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM Products ")
+    connection.close()
+    return cursor.fetchall()
+
+
+if __name__ == "__main__":
+    initiate_db()
